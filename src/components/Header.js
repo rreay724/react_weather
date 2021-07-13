fabbf30cf89ba30462339e76105aacbdab18d83f
Bobby Reay <rreay724@gmail.com>
2021-07-13 11:07:45 -0400

    added header for loading

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,5 @@
-import React, { useState, useContext } from "react";
+import React, { useState } from "react";
 import SearchIcon from "@material-ui/icons/Search";
-import WeatherContext from "../context/weatherContext";
 
 function Header({ data }) {
   const [zipCode, setZipCode] = useState("");
@@ -33,20 +32,35 @@ function Header({ data }) {
       </div>
       <div className="bg-blue-300 h-8">
         <div className="ml-16 flex">
-          <div className="flex">
-            <img
-              className="h-8 w-8"
-              src={
-                "http://openweathermap.org/img/w/" +
-                data.weather[0].icon +
-                ".png"
-              }
-            />
-          </div>
+          {data ? (
+            <div className="flex">
+              <img
+                className="h-8 w-8"
+                src={
+                  "http://openweathermap.org/img/w/" +
+                  data.weather[0].icon +
+                  ".png"
+                }
+              />
+            </div>
+          ) : (
+            <div className="flex"></div>
+          )}
 
           <div className="flex mt-1 ml-1 text-white">
-            <p className="">{((data.main.temp * 9) / 5 + 32).toFixed(0)}°</p>
-            <p className="font-bold ml-2">{data.name}</p>
+            {data ? (
+              <>
+                <p className="">
+                  {((data.main.temp * 9) / 5 + 32).toFixed(0)}°
+                </p>
+                <p className="font-bold ml-2">{data.name}</p>
+              </>
+            ) : (
+              <>
+                <p className="">-°</p>
+                <p className="font-bold ml-2">--</p>
+              </>
+            )}
           </div>
         </div>
       </div>
